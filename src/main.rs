e3e91017b97f1b45880a1e9d16bb56f86aaf55b6
Lunush <76045303+lunush@users.noreply.github.com>
2021-02-21 20:05:14 +0300

    Refactoring and --short flag

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,55 @@
+#[macro_use]
+extern crate clap;
+
 use clap::{Arg, App};
+
+async fn get(from: &str, to: &str) -> Result<String, reqwest::Error> {
+    let url = &format!("https://api.exchangeratesapi.io/latest?base={}", &from)[..];
+    let res = reqwest::get(url).await?;
+    let body = res.text().await?;
+    let json: serde_json::Value = serde_json::from_str(&body).expect("The result doesn't seem to be JSON");
+    let to_val = json["rates"][&to].to_string();
+
+    Ok(to_val)
+}
 
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
-let cli = App::new("Coin")
-                          .version("0.1.0")
-                          .about("Get latest exchange rates")
-                          .arg(Arg::with_name("FROM")
+let cli = App::new("Rates")
+                          .version(crate_version!())
+                          .about(crate_description!())
+                          .author(crate_authors!())
+                          .arg(Arg::with_name("from")
                                .help("Currency you want to convert from")
                                .required(true)
                                .index(1))
-                          .arg(Arg::with_name("TO")
+                          .arg(Arg::with_name("to")
                                .help("Currency you want to convert to")
                                .required(true)
                                .index(2))
+                          .arg(Arg::with_name("amount")
+                               .short("a")
+                               .long("amount")
+                               .takes_value(true)
+                               .help("Amount of a given currency. Defaults to 1"))
+                          .arg(Arg::with_name("short")
+                               .short("s")
+                               .long("short")
+                               .help("Show only the result value"))
                           .get_matches();
 
-    let base = String::from(cli.value_of("FROM").unwrap().to_uppercase());
-    let to = String::from(cli.value_of("TO").unwrap().to_uppercase());
-    let url = &format!("https://api.exchangeratesapi.io/latest?base={}", &base)[..];
-    let res = reqwest::get(url).await?;
-    println!("Status: {}", res.status());
+    let amount = cli.value_of("amount").unwrap().parse::<f64>().unwrap();
+    let from = String::from(cli.value_of("from").unwrap().to_uppercase());
+    let to = String::from(cli.value_of("to").unwrap().to_uppercase());
+    let short = cli.is_present("short");
 
-    let body = res.text().await?;
-    let json: serde_json::Value = serde_json::from_str(&body).expect("The result doesn't seem to be JSON");
-    let to_val = json["rates"][&to].to_string().parse::<f32>().unwrap();
-    println!("1 {} = {:?} {}", base, to_val, to);
+    let to_val = get(&from, &to).await.unwrap().parse::<f64>().unwrap() * amount;
+
+    if short == true {
+        println!("{}", to_val);
+    } else {
+        println!("{} {} = {} {}", amount, from, to_val, to);
+    }
 
     Ok(())
 }
